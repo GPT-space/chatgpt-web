c3db10c123ea530e7d0713cbbd4472e1bc3bd10a
mark <wsjzs@hotmail.com>
2023-04-18 03:57:04 +0800

    feat: style

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -46,7 +46,7 @@ const inputRef = ref<Ref | null>(null)
 // 添加PromptStore
 const promptStore = usePromptStore()
 
-// 使用storeToRefs，保证store修改后，联想部分能够重新渲染
+// 使用storeToRefs，保证store修改后，联想部分能够重新渲染 
 const { promptList: promptTemplate } = storeToRefs<any>(promptStore)
 
 // 未知原因刷新页面，loading 状态不会重置，手动重置
